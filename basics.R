8410d3d599aba4a8dd6773be2a3c1fac551824be
Grant West <gwest@uark.edu>
2018-01-11 16:01:22 -0600

    add basics

---
+++ basics.R
@@ -0,0 +1,101 @@
+## WEEK 1
+
+### R Basics
+
+#R console example
+2+3
+2*3
+
+#R editor example
+2+3
+2*3
+
+#R Graphics (Quartz) example
+x<--10:10
+x
+y<-x^2
+y
+plot(x,y)
+
+
+### R Object and R Workspace
+
+#Check R objects in current workspace
+ls()
+
+#Remove R objects in current workspace
+rm(x,y)
+
+#Check
+ls()
+
+
+### R Function
+
+#"Built-in" function
+#sum() function
+sum
+?sum
+sum(1:5)
+
+#length() function
+length
+?length
+length(1:5)
+
+#"User-defined" function
+my.mean<-function(x)(sum(x)/length(x))
+my.mean
+my.mean(1:5)
+
+
+### R Package
+
+#Check R packages in your machine
+library()
+
+#Check R packages currently loaded
+search()
+
+#Download/install/load "car" package
+install.packages("car")
+
+#Get some information on "car" package
+help(package=car)
+
+#Load "car" package to current working environment
+library(car)
+
+#Check
+search()
+
+
+### Current Working Directory
+
+#Check current working directory
+getwd()
+
+#Set new working directory
+#Windows
+setwd("C:/Users/Username/Desktop/")
+
+#Mac OS
+setwd("/Users/Username/Desktop/")
+
+#Check
+getwd()
+
+
+### Getting Help
+
+# Help for a specific topic (e.g., regression)  
+??regression
+
+# Information on specific function
+?length
+
+# Information on specific package (functions and data sets)
+help(package=car)
+
+# Examples of the use of specific function
+example(length)
